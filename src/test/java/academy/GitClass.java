96855e302987cdc5a27ae574dde35b323118f9a6
ANUMANTHU <anumanthunitt@gmail.com>
2018-08-05 14:49:59 +0530

    Added GITCLASS2

--- src/test/java/academy/GitClass.java
+++ src/test/java/academy/GitClass.java
@@ -19,4 +19,35 @@ public class GitClass {
 		System.out.println("This is my gitmethod2");
 	}
 
+	public void gitmethod3()
+	{
+		System.out.println("This is my gitmethod3");
+	}
+	
+}
+
+class GitClass2 {
+
+	public static void main(String[] args) {
+	
+		
+	
+
+	}
+	
+	public void gitmethod1()
+	{
+		System.out.println("This is my gitmethod Git Class 2");
+	}
+	
+	public void gitmethod2()
+	{
+		System.out.println("This is my gitmethod2 Git Class 2");
+	}
+
+	public void gitmethod3()
+	{
+		System.out.println("This is my gitmethod3 Git Class 2");
+	}
+	
 }
